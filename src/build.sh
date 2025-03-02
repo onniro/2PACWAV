80ba7250a51b8e2ee48b15b31fd41bc9d3e52ef3
onniro <onnirokka@gmail.com>
2025-03-03 01:54:18 +0200

    attempt at mangling some dependencies

--- src/build.sh
+++ src/build.sh
@@ -13,7 +13,7 @@ CODEC_DIR="$BASEDIR/3rd_party/codecs"
 
 LIB_DIRS="-L$BASEDIR/3rd_party/SDL2/lib -L$BASEDIR/3rd_party/codecs/lib"
 OBJ_FILES=""
-LINK_LIBS="-l:libSDL2.a -lm -lGL $SDL_DIR/lib/libSDL2_mixer.a $SDL_DIR/lib/libSDL2_mixer.a $BASEDIR/lib/libnuklear_sdl2_gl2.a"
+LINK_LIBS="$SDL_DIR/lib/libSDL2.a -lm -lGL $SDL_DIR/lib/libSDL2_mixer.a $BASEDIR/lib/libnuklear_sdl2_gl2.a -lbsd"
 WARNINGS="-Wall -Wpedantic -Wno-pointer-arith -Wno-unused-variable -Wno-unused-function -Wno-unused-but-set-variable -Wno-write-strings -Wno-format"
 DEFINES="-D_2PACWAV_DEBUG=1 -D_2PACWAV_LINUX=1"
 
