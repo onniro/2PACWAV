1a77de5cf8009e7d0d10919b89bb86f17f57591f
onniro <onnirokka@gmail.com>
2025-03-22 19:39:34 +0200

    add release mode script

--- src/release.sh
+++ src/release.sh
@@ -1,10 +1,11 @@
+
 #!/bin/sh
 
 BASEDIR="$PWD/.."
 
 SOURCES="$PWD/linux_2pacwav.cpp $PWD/2pacwav.cpp"
 INCLUDE_DIRS="-I$BASEDIR/3rd_party/SDL2 -I$BASEDIR/3rd_party/SDL2/include -I$BASEDIR/3rd_party -I$BASEDIR/3rd_party/nuklear -I$BASEDIR/3rd_party/id3v2lib/include -I$BASEDIR/3rd_party/taglib/include -I$BASEDIR/3rd_party/taglib/include/mpeg/id3v2/ -I$BASEDIR/3rd_party/taglib/include/mpeg/" 
-COMP_FLAGS="-O0 -gdwarf"
+COMP_FLAGS="-O2 -gdwarf"
 LINK_FLAGS="-o 2pacwav"
 
 SDL_DIR="$BASEDIR/3rd_party/SDL2"
@@ -14,9 +15,9 @@ LIB_DIRS="-L$BASEDIR/3rd_party/SDL2/lib -L$BASEDIR/3rd_party/codecs/lib -L$BASED
 OBJ_FILES=""
 LINK_LIBS="$SDL_DIR/lib/libSDL2.a -lm -lGL $SDL_DIR/lib/libSDL2_mixer.a $BASEDIR/lib/libnuklear_sdl2_gl2.a -l:libtag.a -l:libz.a -l:libbsd.a"
 WARNINGS="-Wall -Wpedantic -Wextra -Wno-unused-parameter -Wno-pointer-arith -Wno-unused-variable -Wno-unused-function -Wno-unused-but-set-variable -Wno-write-strings -Wno-format"
-DEFINES="-D_2PACWAV_DEBUG=1 -D_2PACWAV_LINUX=1 -DPAC_SAMPLE_RATE=48000"
+DEFINES="-D_2PACWAV_RELEASE=1 -D_2PACWAV_LINUX=1 -DPAC_SAMPLE_RATE=48000"
 
-WORKDIR="$BASEDIR/build/linux_x64_debug"
+WORKDIR="$BASEDIR/build/linux_x64_release"
 
 CMDLINE="g++ $DEFINES $INCLUDE_DIRS $LIB_DIRS $WARNINGS $COMP_FLAGS $SOURCES $LINK_LIBS $LINK_FLAGS $OBJ_FILES" 
 
