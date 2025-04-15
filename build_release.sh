1d55c08a2bfb136eabf45e7e8b21487343c714de
onniro <onnirokka@gmail.com>
2025-04-15 15:56:25 +0300

    change some link options

--- build_release.sh
+++ build_release.sh
@@ -16,7 +16,7 @@ CODEC_DIR="$BASEDIR/3rd_party/codecs"
 LIB_DIRS="-L$BASEDIR/3rd_party/SDL2/lib -L$BASEDIR/3rd_party/taglib/lib"
 OBJ_FILES="$BASEDIR/lib/nuklear_sdl2_gl2_implementation.o"
 #LINK_LIBS="$SDL_DIR/lib/libSDL2.a -lm -lGL $SDL_DIR/lib/libSDL2_mixer.a $BASEDIR/lib/libnuklear_sdl2_gl2.a -l:libtag.a -l:libz.a -l:libbsd.a"
-LINK_LIBS="$SDL_DIR/lib/libSDL2.a -lm -lGL $SDL_DIR/lib/libSDL2_mixer.a -l:libtag.a -l:libz.a -l:libbsd.a"
+LINK_LIBS="$SDL_DIR/lib/libSDL2.a -static-libstdc++ -static-libgcc -lm -lGL $SDL_DIR/lib/libSDL2_mixer.a -l:libtag.a -l:libz.a -l:libbsd.a"
 WARNINGS="-Wall -Wpedantic -Wextra -Wno-c99-extensions -Wno-unused-parameter -Wno-pointer-arith -Wno-unused-variable -Wno-unused-function -Wno-unused-but-set-variable -Wno-write-strings -Wno-format"
 DEFINES="-D_2PACWAV_RELEASE=1 -D_2PACWAV_LINUX=1 -DPAC_SAMPLE_RATE=48000"
 
