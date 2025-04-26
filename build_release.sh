51ed4c365a6012c121e529a5e1fe3eb33ca9328d
onniro <onnirokka@gmail.com>
2025-04-26 16:44:17 +0300

    format

--- build_release.sh
+++ build_release.sh
@@ -1,12 +1,33 @@
 #!/bin/sh
 
+echo "build started @ $(date)"
+
 BASEDIR="$PWD"
 SRCDIR="$BASEDIR/src"
 
-sh $PWD/compile_imgui.sh
+COMPILE_IMGUI=1
+
+for arg in "$@"; do
+    if [ "$arg" = "-noobj" ]; then
+        COMPILE_IMGUI=0
+    fi
+done
+
+if [ $COMPILE_IMGUI = 1 ]; then
+    sh $PWD/compile_imgui.sh
+fi 
 
 SOURCES="$SRCDIR/linux_2pacwav2.cpp"
-INCLUDE_DIRS="-I$BASEDIR/3rd_party/SDL2 -I$BASEDIR/3rd_party/SDL2/include -I$BASEDIR/3rd_party/imgui -I$BASEDIR/3rd_party -I$BASEDIR/3rd_party/id3v2lib/include -I$BASEDIR/3rd_party/taglib/include -I$BASEDIR/3rd_party/taglib/include/mpeg/id3v2/ -I$BASEDIR/3rd_party/taglib/include/mpeg/" 
+
+INCLUDE_DIRS="-I$BASEDIR/3rd_party/SDL2 \
+        -I$BASEDIR/3rd_party/SDL2/include \
+        -I$BASEDIR/3rd_party/imgui \
+        -I$BASEDIR/3rd_party \
+        -I$BASEDIR/3rd_party/id3v2lib/include \
+        -I$BASEDIR/3rd_party/taglib/include \
+        -I$BASEDIR/3rd_party/taglib/include/mpeg/id3v2/ \
+        -I$BASEDIR/3rd_party/taglib/include/mpeg/" 
+
 COMP_FLAGS="-O3 -gdwarf"
 EXE_NAME="2w2"
 LINK_FLAGS="-o $EXE_NAME"
@@ -16,14 +37,34 @@ CODEC_DIR="$BASEDIR/3rd_party/codecs"
 
 LIB_DIRS="-L$BASEDIR/3rd_party/SDL2/lib -L$BASEDIR/3rd_party/taglib/lib"
 OBJ_FILES="$BASEDIR/lib/imgui*.o"
-#LINK_LIBS="$SDL_DIR/lib/libSDL2.a -lm -lGL $SDL_DIR/lib/libSDL2_mixer.a $BASEDIR/lib/libnuklear_sdl2_gl2.a -l:libtag.a -l:libz.a -l:libbsd.a"
-LINK_LIBS="$SDL_DIR/lib/libSDL2.a -static-libstdc++ -static-libgcc -lm -lGL $SDL_DIR/lib/libSDL2_mixer.a -l:libtag.a -l:libz.a -l:libbsd.a"
-WARNINGS="-Wall -Wpedantic -Wextra -Wno-c99-extensions -Wno-unused-parameter -Wno-pointer-arith -Wno-unused-variable -Wno-unused-function -Wno-unused-but-set-variable -Wno-write-strings -Wno-format"
+
+LINK_LIBS="$SDL_DIR/lib/libSDL2.a \
+        -static-libstdc++ \
+        -static-libgcc \
+        -lm \
+        -lGL \
+        $SDL_DIR/lib/libSDL2_mixer.a \
+        -l:libtag.a \
+        -l:libz.a"
+
+WARNINGS="-Wall -Wpedantic -Wextra -Wno-c99-extensions \
+        -Wno-unused-parameter -Wno-pointer-arith \
+        -Wno-unused-variable -Wno-unused-function \
+        -Wno-unused-but-set-variable -Wno-write-strings -Wno-format"
+
 DEFINES="-D_2PACWAV_RELEASE=1 -D_2PACWAV_LINUX=1 -DPAC_SAMPLE_RATE=48000"
 
 WORKDIR="$BASEDIR/build/linux_x64_release"
 
-CMDLINE="clang++ $DEFINES $INCLUDE_DIRS $LIB_DIRS $WARNINGS $COMP_FLAGS $SOURCES $LINK_LIBS $LINK_FLAGS $OBJ_FILES" 
+CMDLINE="clang++ $DEFINES \
+        $INCLUDE_DIRS \
+        $LIB_DIRS \
+        $WARNINGS \
+        $COMP_FLAGS \
+        $SOURCES \
+        $LINK_LIBS \
+        $LINK_FLAGS \
+        $OBJ_FILES" 
 
 mkdir -p $WORKDIR; cd $WORKDIR
 echo $CMDLINE; $CMDLINE
