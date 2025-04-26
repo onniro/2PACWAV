406a16f72d5486f5671a557fde3ecae07689c53d
onniro <onnirokka@gmail.com>
2025-04-26 16:59:46 +0300

    format

--- compile_imgui.sh
+++ compile_imgui.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 SOURCES="$PWD/3rd_party/imgui/*.cpp"
-INCLUDE_DIRS="-I$PWD/3rd_party/SDL2 -I$PWD/3rd_party/SDL2/include -I$PWD/3rd_party/imgui" 
+INCLUDE_DIRS="-I$PWD/3rd_party/SDL2 \
+        -I$PWD/3rd_party/SDL2/include \
+        -I$PWD/3rd_party/imgui" 
 COMP_FLAGS="-O3 -gdwarf"
 
+CMDLINE="clang++ $INCLUDE_DIRS $COMP_FLAGS -c $SOURCES"
+
 mkdir -p $PWD/lib; cd $PWD/lib
-clang++ $INCLUDE_DIRS $COMP_FLAGS -c $SOURCES
+echo $CMDLINE; 
+$CMDLINE
 cd ..
