7a26daeacde3ca7230d647fbe92af100a3936525
onniro <onnirokka@gmail.com>
2025-06-18 23:52:51 +0300

    remove leftover stuff that isn't used anymore

--- 3rd_party/taglib/include/CMakeFiles/tag.dir/toolkit/tdebug.cpp.o.d
+++
@@ -1,3 +0,0 @@
-taglib/CMakeFiles/tag.dir/toolkit/tdebug.cpp.o: \
- /home/onni/source/libs/taglib-2.0.2/taglib/toolkit/tdebug.cpp \
- /usr/include/stdc-predef.h /home/onni/source/libs/taglib-2.0.2/config.h
--- 3rd_party/taglib/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/onni/source/libs/taglib-2.0.2/taglib
-# Build directory: /home/onni/source/libs/taglib-2.0.2/taglib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
