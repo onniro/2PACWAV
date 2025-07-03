817d01fefc381f6dbc9fae651bfa82acba6dfd72
onniro <onnirokka@gmail.com>
2025-07-03 18:10:08 +0300

    bump version

--- src/2pacwav2.h
+++ src/2pacwav2.h
@@ -14,14 +14,14 @@ Date: Thu 24 Apr 2025 04:34:59 PM EEST
 
 #define _2PACWAV_VER_MAJOR      (0)
 #define _2PACWAV_VER_MINOR      (11)
-#define _2PACWAV_VER_PATCH      (20)
+#define _2PACWAV_VER_PATCH      (21)
 
 #define PAC_INLINE static inline
 #define PAC_INTERNAL static
 #define PAC_LOCAL_STATIC static
 
-#define WINDOW_WIDTH    1280
-#define WINDOW_HEIGHT   800
+#define WINDOW_WIDTH    (1280)
+#define WINDOW_HEIGHT   (800)
 #define MAX_FRAMETIME_MICROSEC ((useconds_t)11111) //90fps
 #define PAC_SEEK_VALUE_MAX (1000.0f)
 
